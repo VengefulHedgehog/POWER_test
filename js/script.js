08a1c6d04ef26cfad328c16fd3b89e53e8486b3a
KuzminykhEM <kuzja-280599@mail.ru>
2022-10-05 14:46:58 +0300

    mobile version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// ------------------slider---------------------
+
 $(document).ready(function(){
     $('.slider').slick();
 });
+
+// ------------------timer---------------------
+
+let time = 1800;
+const timer = document.querySelector('.timer__display');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const min = Math.floor(time/60);
+    let sec = time % 60;
+    sec = sec < 10 ? "0" + sec: sec;
+    timer.innerHTML = `${min} : ${sec}`;
+    time--;
+}
